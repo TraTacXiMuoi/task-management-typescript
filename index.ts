97f0dc49d7904d5ad08b31e53f5de9349802b0ca
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-03-16 21:35:11 +0700

    8.3. Lấy ra chi tiết công việc

--- index.ts
+++ index.ts
@@ -18,6 +18,19 @@ app.get("/tasks", async (req: Request, res: Response) => {
   res.json(tasks);
 });
 
+app.get("/tasks/detail/:id", async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+
+  console.log(id);
+
+  const task = await Task.findOne({
+    _id: id,
+    deleted: false
+  });
+
+  res.json(task);
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
